5cc0e9245d3a6eeca1ad7a01b06a344333659298
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-07 14:31:48 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/davinci/ejerciciosbado/MainActivity.java
@@ -0,0 +1,16 @@
+package ar.edu.davinci.ejerciciosbado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
